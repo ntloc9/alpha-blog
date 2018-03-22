4c002aac1f30a452ad1e5f666191dd15b8bbec62
loc <steven.ngtloc@gmail.com>
2018-03-22 08:42:02 +0000

    create show, edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,14 @@
 class ArticlesController < ApplicationController
+   def index
+      @articles = Article.all
+   end
    def new
        @article = Article.new
    end
+   def edit
+      @article = Article.find(params[:id])
+   end
+   
    
    def create
       @article = Article.new(article_params)
@@ -12,9 +19,22 @@ class ArticlesController < ApplicationController
          render 'new'
       end
    end
+   
+   def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+         flash[:notice] = "Article sucessfully updated"
+         redirect_to article_path(@article)
+      else
+         render 'edit'
+      end
+   end
+   
    def show
       @article = Article.find(params[:id])
    end
+   
+
    private
    def article_params
    params.require(:article).permit(:title, :description)
